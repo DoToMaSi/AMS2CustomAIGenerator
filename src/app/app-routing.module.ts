5ee1dae51cc5fd863dd6ede644350531f08973d4
Douglas Tomacheski de Abreu e Silva <douglas.silva@gestran.com.br>
2022-07-15 15:41:57 -0300

    - Fixes to routing (avoids crashes on Heroku)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
 ];
